cff7c05e832255805747bd88236ad096b20085fc
Troy Willmot <yort@yortondotnet.com>
2016-10-10 14:17:05 +1300

    Update publish bat for new version.

--- PublishNugetPackage.cmd
+++ PublishNugetPackage.cmd
@@ -1,6 +1,6 @@
 @echo off
 echo Press any key to publish
 pause
-".nuget\NuGet.exe" push Spooky.1.0.0.0.nupkg -Source https://api.nuget.org/v3/index.json
-".nuget\NuGet.exe" push Spooky.Json20.1.0.0.0.nupkg -Source https://api.nuget.org/v3/index.json
+".nuget\NuGet.exe" push Spooky.1.0.0.1.nupkg -Source https://api.nuget.org/v3/index.json
+".nuget\NuGet.exe" push Spooky.Json20.1.0.0.1.nupkg -Source https://api.nuget.org/v3/index.json
 pause
